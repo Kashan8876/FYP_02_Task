cdd27c3e641f3ec4e68c1ca7fe546b3d968848f5
Muhammad Kashan <63954509+Kashan8876@users.noreply.github.com>
2021-02-08 17:00:32 +0500

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,3 @@
+
+echo PORT $PORT
+web: sh setup.sh && streamlit run --server.port $PORT Task_FYP_02.py
---
+++ Task_FYP_02.py
@@ -0,0 +1,14 @@
+import pickle
+import streamlit as st
+from flask import request
+
+def main():
+    st.title('Smart Health Consultant')
+    st.text('Hello World')
+
+
+
+
+if __name__=='__main__':
+
+    main()
